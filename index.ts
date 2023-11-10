e252c2d1e23151d14aa10f8ddf126541efce140d
geranton93 <Xlive93@yandex.ru>
2023-11-10 11:48:07 +0200

    improve algorithm

--- index.ts
+++ index.ts
@@ -1,44 +1,43 @@
-
 /**
  * Recursively finds paths in a list of objects based on a specified target key and value.
  *
  * @export
  * @template T
  * @param {T[]} objects - An array of objects to search through.
- * @param {string} targetKey - The key to search for in the objects.
- * @param {(string | number | boolean)} targetValue - The value to find in the objects based on the target key.
- * @param {string} childrenProperty - The property name representing the children of each object.
- * @returns {*}  {T[]} An array of objects representing the path from the root to the target.
+ * @param {keyof T} targetKey - The key to search for in the objects.
+ * @param {string | number | boolean} targetValue - The value to find in the objects based on the target key.
+ * @param {keyof T} childrenProperty - The property name representing the children of each object.
+ * @returns {Omit<T, typeof childrenProperty>[]} An array of objects representing the path from the root to the target.
  */
 export function returnTreeObjectsPath<T>(
   objects: T[],
-  targetKey: string,
+  targetKey: keyof T,
   targetValue: string | number | boolean,
-  childrenProperty: string
-): T[] {
-  const result: T[] = [];
+  childrenProperty: keyof T,
+): Omit<T, typeof childrenProperty>[] {
+  const result: Omit<T, typeof childrenProperty>[] = [];
 
-  function findRecursively(obj: any): boolean {
+  function findRecursively(obj: T): boolean {
     if (obj[targetKey] === targetValue) {
       const { [childrenProperty]: _, ...rest } = obj;
-      result.push(rest);
-      return true; // Added to stop further recursion
+      result.unshift(rest);
+      return true;
     }
 
-    if (obj[childrenProperty] && obj[childrenProperty].length > 0) {
-      for (const child of obj[childrenProperty]) {
+    if (obj[childrenProperty] && Array.isArray(obj[childrenProperty])) {
+      for (const child of obj[childrenProperty] as T[]) {
         if (findRecursively(child)) {
           const { [childrenProperty]: _, ...rest } = obj;
-          result.push(rest);
-          return true; // Added to stop further recursion
+          result.unshift(rest);
+          return true;
         }
       }
     }
 
-    return false; // Added for cases where targetValue is not found
+    return false;
   }
 
-  objects.forEach((obj) => findRecursively(obj));
+  objects.some(findRecursively);
 
-  return result.reverse(); // Reverse the result to get the path from root to target
+  return result;
 }
